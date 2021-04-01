a5efc612a7aa7ef654b67ec98d99a90cd841dda6
Rebecca Chen <rebecca061996@gmail.com>
2021-04-01 16:40:16 -0400

    read exercise using a function to read a file and looping through each line to get only the name

---
+++ cast_list.py
@@ -0,0 +1,10 @@
+def create_cast_list(filename):
+    cast_list = []
+    with open(filename) as f:
+        for line in f:
+            cast_list.append(line.split(',')[0])
+    return cast_list
+
+cast_list = create_cast_list('flying_circus_cast.txt')
+for actor in cast_list:
+    print(actor)
