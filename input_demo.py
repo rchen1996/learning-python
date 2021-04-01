78f4817c27ea366585c824cf89bca5816553a760
Rebecca Chen <rebecca061996@gmail.com>
2021-04-01 15:38:19 -0400

    first commit with initial python script tests

---
+++ input_demo.py
@@ -0,0 +1,12 @@
+name = input('Enter a name: ')
+print('Hello', name.title())
+
+num = int(input('Enter a number: '))
+num += 20
+print(num)
+
+x = eval(input('Enter an expression: '))
+print(x)
+
+x = eval('num + 42')
+print(x)
